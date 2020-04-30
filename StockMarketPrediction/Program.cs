50cfe971cd2ce04fa649d4caec0c67b0a9a1170f
hs115488 <haresh.singh.97@gmail.com>
2020-04-30 00:45:08 -0400

    Added database connection

---
+++ StockMarketPrediction/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Data.SqlClient;
+namespace StockMarketPrediction
+{
+    /*
+        CODE IS BROKEN 
+        DO NOT RUN
+    */
+
+    class Program
+    {
+        static void db_query(String query)
+        {
+            
+            SqlConnection connection = new SqlConnection("Data Source = mydb.cueaveoortkb.us - east - 1.rds.amazonaws.com, 1433; Initial Catalog = dow_jones; User ID = root; Password = ***********");
+            connection.Open();
+            
+            SqlCommand cmd = new SqlCommand(query, connection);
+            SqlDataReader reader = cmd.ExecuteReader();
+            
+            Console.WriteLine(reader);
+        }
+        static void Main(string[] args)
+        {
+            db_query("select * from YearPrice");
+
+        }
+    }
+}
