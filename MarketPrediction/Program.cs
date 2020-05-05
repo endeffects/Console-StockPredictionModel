1f70b4b5efc345f5f783f8571d7ae8370d0639bc
hs115488 <haresh.singh.97@gmail.com>
2020-05-04 23:15:40 -0400

    Finished Model

---
+++ MarketPrediction/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using MarketPredictionML.Model;
+
+namespace MarketPrediction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = new ModelInput();
+            ModelOutput result = ConsumeModel.Predict(input);
+        }
+    }
+}
